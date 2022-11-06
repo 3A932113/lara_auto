42f6bcff84249a8507c62b928b14da044341c4a3
Yang shu mei <pbs71129@gmail.com>
2022-11-06 23:03:33 +0800

    new push

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ require '../vendor/autoload.php';
 // 類別先import進來
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
 $lara= new Lara();
 $vincent= new Someone('Vincent');
 // 類別未先import進來
@@ -11,3 +16,11 @@ $john= new Demo\Hello\Someone('John');
 $hello= new Demo\HelloWorld();
 use Demo\HelloWorld as World; // 類別另取別名
 $world = new World();
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
